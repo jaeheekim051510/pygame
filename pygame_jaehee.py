b91002ef734848ccb808f2b4911880cffc8c5b2f
Jaehee Kim <jaeheekim@Jaehees-MacBook-Pro.local>
2018-02-12 10:33:33 -0500

    make other_cars as a sub class Sprite

--- pygame_jaehee.py
+++ pygame_jaehee.py
@@ -1,29 +1,51 @@
 
 import pygame, sys
 from pygame.locals import *
+from pygame.sprite import Sprite
+import random
 
 clock = 30
 mainClock = pygame.time.Clock()
 
 class User_car(pygame.sprite.Sprite):
     def __init__(self, screen):
-        super(User_car,self).__init__()
+        super(User_car,self).__init__() #Don't know what it does yet.
         self.user_carx = 150
         self.user_cary = 380
-        self.image = pygame.image.load("Audi.png").convert_alpha()
+        self.screen = screen 
+        self.image = pygame.image.load("image/Audi.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (160, 170))
         self.rect = self.image.get_rect()
-        self.screen = screen #resizing image 
-        self.resizedcar = pygame.transform.scale(self.image, (160, 170))
 
-# class Police_car(pyagme.sprite.Sprite):
-#     def __init__(self, x, y, speed):
-#         super(Police_car,self).__init__()
-#         self.police_carx = x
-#         self.police_cary = y
-#         self.speed = speed
-#     def update (self, )
+class Other_cars(pygame.sprite.Sprite):
+    def __init__(self, speed):
+        super(Other_cars,self).__init__()
+        self.x = random.randrange(215, 680, 100)
+        self.y = 100
+        self.speed = speed
+
+    def update (self, screen_x, screen_y):
+        self.x += self.speed
+        self.y += self.speed
+        if self.x > screen_x:
+            self.x = 0
+        if self.y > screen_y:
+            self.y = 0
+    
+    def display(self, screen):
+        # Police image
+        police  = pygame.image.load("image/police1.png").convert_alpha()
+        police = pygame.transform.scale(police, (160,170))
+        # # Ambulance image
+        # ambulance = pygame.image.load("image/ambulance1.png").convert_alpha()
+        # ambulance = pygame.transform.scale(ambulance, (160,170))
+        
+        screen.blit(police, (self.x, self.y))
+    
+    #     self.rect = self.image.get_rect()
 
 def main():
+    # declare the size of the canvas
     screen_x = 1000
     screen_y = 600
 
@@ -31,17 +53,27 @@ def main():
     screen = pygame.display.set_mode((screen_x,screen_y))
     pygame.display.set_caption('My first Game')
 
-    background = pygame.image.load('background-1.png')
+    # Background image
+    background = pygame.image.load('image/background-1.png')
     background = pygame.transform.scale(background, (screen_x,screen_y))
 
-    car = User_car(screen)
+    # othercar_list = [
+    #     police, 
+    #     ambulance
+    # ]
 
-    while True:
+    car = User_car(screen)
+    other_car = Other_cars(screen)
+
+    stop_game = False
+    while not stop_game:
         for event in pygame.event.get():
             if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+                # pygame.quit()
+                # sys.exit()
+                stop_game = True
 
+            #User_car Key Function 
             if event.type == KEYDOWN: 
                 if event.key == K_RIGHT:
                     if car.user_carx <= 680:
@@ -63,12 +95,16 @@ def main():
                         car.user_cary += 50
                     else:
                         car.user_cary += 0
+            # Other cars logic
+            # for othercar in othercar_list:
+            #     othercar.display(screen)
         
         screen.blit(background,(0,0))
-        screen.blit(car.resizedcar, (car.user_carx, car.user_cary))
+        screen.blit(car.image, (car.user_carx, car.user_cary))
         pygame.display.update()
         mainClock.tick(clock)
     
-    pygmae.quit()
+    pygame.quit()
+
 if __name__ == '__main__':
     main()
